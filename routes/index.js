2b805d1d4d40404a17cebf990b2a77debd79b139
David <daviding92@gmail.com>
2022-09-01 19:27:10 -0700

    added test calc

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({message: 'alive'});
+	const gen = Generations.get(5); // alternatively: const gen = 5;
+	const result = calculate(
+	  gen,
+	  new Pokemon(gen, 'Gengar', {
+		item: 'Choice Specs',
+		nature: 'Timid',
+		evs: {spa: 252},
+		boosts: {spa: 1},
+	  }),
+	  new Pokemon(gen, 'Chansey', {
+		item: 'Eviolite',
+		nature: 'Calm',
+		evs: {hp: 252, spd: 252},
+	  }),
+	  new Move(gen, 'Focus Blast')
+	);
+
+	console.log(result);
+    res.json({message: result});
 });
 
 module.exports = router;
